25c9f74bc2762972ced39f5e89d89bca79651b6b
Keira Nevers <keira.nevers@gmail.com>
2018-03-25 09:59:30 -0400

    push working app

---
+++ controllers/burgersController.js
@@ -0,0 +1,34 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get('/', function (req, res) {
+  res.redirect('/burgers');
+});
+
+router.get('/burgers', function (req, res) {
+  burger.selectAll(function (data) {
+    var hbsObject = { burgers: data };
+    res.render('index', hbsObject);
+  });
+});
+
+
+router.post('/burgers/add', function (req, res) {
+  burger.insertOne('burger_name', req.body.burger_name, function () {
+    res.redirect('/burgers');
+  });
+});
+
+router.put('/burgers/devour/:id', function (req, res) {
+  var condition = 'id = ' + req.params.id;
+  burger.updateOne({ devoured: req.body.devoured }, condition, function () {
+    res.redirect('/burgers');
+  });
+});
+
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,22 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+  selectAll: function (cb) {
+    orm.selectAll('burgers', function (res) {
+      cb(res);
+    });
+  },
+  insertOne: function (col, val, cb) {
+    orm.insertOne('burgers', col, val, function (res) {
+      cb(res);
+    });
+  },
+  updateOne: function (objColVal, condition, cb) {
+    orm.updateOne('burgers', objColVal, condition, function (res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
